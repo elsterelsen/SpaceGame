6263a3f2484f6c333446de31216704fd48bdc85b
Elija <elsterelsen@gmail.com>
2021-09-01 19:45:39 +0200

    Added HUD and Minimap class

---
+++ core/src/com/space/game/hud/HUD.java
@@ -0,0 +1,7 @@
+package com.space.game.hud;
+
+public class HUD {
+    private float playerHp;
+    private Minimap minimap;
+
+}
---
+++ core/src/com/space/game/hud/Minimap.java
@@ -0,0 +1,11 @@
+package com.space.game.hud;
+
+import com.badlogic.gdx.utils.Array;
+import com.space.game.entities.Entity;
+import com.space.game.playerClasses.Player;
+
+public class Minimap {
+    Player player;
+    Array<Entity> entities;
+
+}
